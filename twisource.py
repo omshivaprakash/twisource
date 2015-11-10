f6db254ae235e1a487fd9b304d4e9184bbbaab49
Sergey Valiev <svaliev@aligntech.com>
2015-11-10 12:43:43 +0300

    Fixed mixed indentation in python source file

--- twisource.py
+++ twisource.py
@@ -11,32 +11,32 @@ from pytz import timezone
 import datetime
 import yaml
 try:
-        import json
+    import json
 except ImportError:
-        import simplejson as json
+    import simplejson as json
 from collections import defaultdict
 
 tz = 'Europe/Moscow'
 tweet_file = 'tweets.yml'
 
 def shorten(url):
-	headers = {'content-type': 'application/json'}
-	payload = {"longUrl": url}
-	url = "https://www.googleapis.com/urlshortener/v1/url"
-	r = requests.post(url, data=json.dumps(payload), headers=headers)
-	link = json.loads(r.text)['id']
-	return link
+    headers = {'content-type': 'application/json'}
+    payload = {"longUrl": url}
+    url = "https://www.googleapis.com/urlshortener/v1/url"
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    link = json.loads(r.text)['id']
+    return link
 
 def lint(message, date, account):
     if len(message) > 140:
-       print "[DEBUG] The text of your tweet scheduled at", date, "is too long"
-       return 1
+        print "[DEBUG] The text of your tweet scheduled at", date, "is too long"
+        return 1
     return 0
-	
+
     f = "settings-" + account + ".json"
     if not (os.path.isfile(f) and os.access(f, os.R_OK)):
-       print "[DEBUG] File with credentials is not accessible"
-       return 1
+        print "[DEBUG] File with credentials is not accessible"
+        return 1
     return 0
 
 def shorten(url):
@@ -61,32 +61,32 @@ def tweeter(message, account, mode):
     api = tweepy.API(auth)
     rt_id = getRTid(message)
     if rt_id:
-       print "[DEBUG] %s: RT https://twitter.com/statuses/%s" % (account, rt_id)
+        print "[DEBUG] %s: RT https://twitter.com/statuses/%s" % (account, rt_id)
     else:
-       print "[DEBUG] %s: %s" % (account, message)
-    if not rt_id:
-       if mode:
-          print "[DEBUG] Posting a message", message
-          api.update_status(message)
-          print "[DEBUG] Posted"
-    else:
-       if mode:
-          print "[DEBUG] Retweeting a message", rt_id
-          api.retweet(rt_id)
-          print "[DEBUG] Posted"
+        print "[DEBUG] %s: %s" % (account, message)
+        if not rt_id:
+            if mode:
+                print "[DEBUG] Posting a message", message
+                api.update_status(message)
+                print "[DEBUG] Posted"
+            else:
+                if mode:
+                    print "[DEBUG] Retweeting a message", rt_id
+                    api.retweet(rt_id)
+                    print "[DEBUG] Posted"
 
 def main(mode):
     with open(tweet_file, 'r') as f:
-         tweets = yaml.load(f)
+        tweets = yaml.load(f)
 
     d = datetime.datetime.now(timezone(tz))
     print "[DEBUG] Time in timezone %s - %s" % (tz, d.strftime("%Y-%m-%d %H:%M"))
     for t in tweets:
         if t['date'] >= d.strftime("%Y-%m-%d %H:%M") and not lint(t['text'], t['date'], t['account']) and not mode:
-           tweeter(t['text'], t['account'], mode)
-        if t['date'] == d.strftime("%Y-%m-%d %H:%M") and not lint(t['text'], t['date'], t['account']) and mode:
-           tweeter(t['text'], t['account'], mode)
-    f.close
+            tweeter(t['text'], t['account'], mode)
+            if t['date'] == d.strftime("%Y-%m-%d %H:%M") and not lint(t['text'], t['date'], t['account']) and mode:
+                tweeter(t['text'], t['account'], mode)
+                f.close
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Post Twitter messages from file.')
@@ -94,8 +94,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.publish:
-       print "[DEBUG] publish mode enabled"
+        print "[DEBUG] publish mode enabled"
     else:
-       print "[DEBUG] lint mode enabled"
+        print "[DEBUG] lint mode enabled"
 
     main(mode = args.publish)
